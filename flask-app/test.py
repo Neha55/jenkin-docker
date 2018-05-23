7f651907637eeec9896d4243a43999d01fc9d27e
Neha Bidkar <nebidkar@in.ibm.com>
2018-05-23 14:12:50 +0530

    update

--- flask-app/test.py
+++ flask-app/test.py
@@ -12,7 +12,7 @@ class TestStringMethods(unittest.TestCase):
         
     def test_upper_1(self):
         print("test_upper...1")
-        self.assertEqual('FOO', 'FOO')
+        self.assertEqual('foo', 'FOO')
         
     def test_upper_2(self):
         print("test_upper...2")
