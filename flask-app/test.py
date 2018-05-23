1d04a70b8c517c76b6700d3713de87c214034e8c
Neha Bidkar <nebidkar@in.ibm.com>
2018-05-23 14:11:05 +0530

    update

--- flask-app/test.py
+++ flask-app/test.py
@@ -2,12 +2,21 @@ import unittest
 
 class TestStringMethods(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        pass
+        
+    @classmethod
+    def tearDownClass(cls):
+        print("I am tear-down")
+        
     def test_upper_1(self):
         print("test_upper...1")
-        self.assertEqual('foo', 'FOO')
+        self.assertEqual('FOO', 'FOO')
+        
     def test_upper_2(self):
         print("test_upper...2")
         self.assertEqual('foo'.upper(), 'FOO')
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2, failfast=True)
