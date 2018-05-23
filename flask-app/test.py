63a81729d2e71ca9314a0ecb1481a431d076c0e3
Neha Bidkar <nebidkar@in.ibm.com>
2018-05-23 14:07:04 +0530

    update

--- flask-app/test.py
+++ flask-app/test.py
@@ -2,9 +2,12 @@ import unittest
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_upper(self):
-        print("test_upper...")
+    def test_upper_1(self):
+        print("test_upper...1")
         self.assertEqual('foo', 'FOO')
+    def test_upper_2(self):
+        print("test_upper...2")
+        self.assertEqual('foo'.upper(), 'FOO')
 
 if __name__ == '__main__':
     unittest.main()
